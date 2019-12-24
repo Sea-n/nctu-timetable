2ae5914b7b933a97c20096060e50b1e6b42326b3
Sean Wei <me@sean.taipei>
2019-12-24 20:50:02 +0800

    Dec 24  20:50

--- update.sh
+++ update.sh
@@ -4,8 +4,6 @@ cd `dirname $0`
 from=${1:-107}
 to=${2:-109}
 
-k=0
-
 for y in `seq $from $to`; do
 	for t in "1" "2" "X"; do
 		(
@@ -20,19 +18,13 @@ for y in `seq $from $to`; do
 			-d 'm_grade=**' \
 			-d 'm_class=**' \
 			-d 'm_option=crsname' \
+			-d 'm_crsoutline=' \
 			-d 'm_crsname= ' \
-			-d 'm_teaname=**' \
-			-d 'm_cos_id=**' \
-			-d 'm_cos_code=**' \
-			-d 'm_crstime=**' \
-			-d 'm_crsoutline=**' \
-			-d 'm_costype=**' > raw-$y-$t.json
+			> raw-$y-$t.json
 
 			jq . raw-$y-$t.json > pretty-$y-$t.json
 			wc pretty-$y-$t.json
 		) &
-		pids[$k]=$!
-		k+=1
 	done
 done
 
@@ -43,7 +35,7 @@ echo "Done."
 find . -size 2c -delete  # Empty raw
 find . -size 3c -delete  # Empty pretty
 echo "Deleted empty file."
-
+exit
 git add .
 git commit -m "`date '+%b %d  %H:%M'`"
 git push
